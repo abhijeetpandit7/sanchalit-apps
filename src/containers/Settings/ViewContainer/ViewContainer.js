4fe845992d08aec9d6401bd8409c771a85b35c83
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2022-08-24 23:24:13 +0530

    Add lazy loading while rendering settings-app view

--- src/containers/Settings/ViewContainer/ViewContainer.js
+++ src/containers/Settings/ViewContainer/ViewContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
 	ABOUT,
 	BALANCE,
@@ -12,16 +12,20 @@ import {
 	TODO,
 } from "../../../utils";
 
+const General = lazy(() => import("./Views/General/General"));
+const Mantras = lazy(() => import("./Views/Mantras/Mantras"));
+
 const getNavItemValue = (navItem) =>
 	SETTINGS_NAV_LIST.find(({ value }) => value === navItem).value;
 
 const SETTINGS_VIEW_LIST = [
 	{
 		value: getNavItemValue(GENERAL),
+		component: <General />,
 	},
 	{ value: getNavItemValue(GENERAL) },
 	{ value: getNavItemValue(TODO) },
-	{ value: getNavItemValue(MANTRAS) },
+	{ value: getNavItemValue(MANTRAS), component: <Mantras /> },
 	{ value: getNavItemValue(PHOTOS) },
 	{ value: getNavItemValue(QUOTES) },
 	{ value: getNavItemValue(LINKS_AND_BOOKMARKS) },
@@ -30,10 +34,23 @@ const SETTINGS_VIEW_LIST = [
 	{ value: getNavItemValue(ABOUT) },
 ];
 
+const Loading = () => (
+	<div className="u--flex-center cp-alpha">
+		<div className="loading">
+			<p className="settings-empty-loading">
+				<i className="loading-icon"></i>
+				Loading...
+			</p>
+		</div>
+	</div>
+);
+
 export const ViewContainer = ({ activeNav }) => (
 	<div className="settings-view-container-wrapper">
 		<div className="settings-view-container">
+			<Suspense fallback={<Loading />}>
 				{SETTINGS_VIEW_LIST.find(({ value }) => value === activeNav).component}
+			</Suspense>
 		</div>
 	</div>
 );
