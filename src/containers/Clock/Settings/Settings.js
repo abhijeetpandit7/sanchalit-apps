adf1b7835a6533d6b9b277db59b18c6652cb9b4e
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2022-08-29 08:47:16 +0530

    Add hour12Clock toggle handler

--- src/containers/Clock/Settings/Settings.js
+++ src/containers/Clock/Settings/Settings.js
@@ -1,18 +1,30 @@
-import React from "react";
-import { toggleSwitch } from "../../../utils";
+import React, { useRef } from "react";
+import { DATA_TEST, HOUR_FORMAT, ON, toggleSwitch } from "../../../utils";
 
-const Settings = () => (
-	<div
-		data-v-c8d4d4da
-		className="dropdown more-dropdown app dash-dropdown dropdown-hide nipple nipple-top-left"
-	>
-		<ul data-v-c8d4d4da className="dropdown-list">
-			<li className="dropdown-list-item has-toggle">
-				<span className="dropdown-list-label">24-hour clock</span>
-				<span className="toggle-slider">{toggleSwitch}</span>
-			</li>
-		</ul>
-	</div>
-);
+const Settings = () => {
+	const dropdownRef = useRef(null);
+
+	const toggleHour12Clock = () =>
+		dropdownRef.current.querySelector(`[${DATA_TEST}]`).classList.toggle(ON);
+
+	return (
+		<div
+			data-v-c8d4d4da
+			className="dropdown more-dropdown app dash-dropdown dropdown-hide nipple nipple-top-left"
+			ref={dropdownRef}
+		>
+			<ul data-v-c8d4d4da className="dropdown-list">
+				<li
+					className="dropdown-list-item has-toggle"
+					data-test={HOUR_FORMAT}
+					onClick={toggleHour12Clock}
+				>
+					<span className="dropdown-list-label">24-hour clock</span>
+					<span className="toggle-slider">{toggleSwitch}</span>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 export default Settings;
