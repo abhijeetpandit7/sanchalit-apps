ab073f0ebacc31bea9795bb3683dc150e353c636
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2022-08-29 21:34:32 +0530

    Add ContextMemo in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import "./App.css";
 import { LogoOverlay } from "./components";
 import { Backgrounds, ModalBase } from "./containers";
@@ -6,32 +6,34 @@ import { useAuthPersist, useUserCustomization } from "./hooks";
 import { TopRow, CenterRegion, CenterBelowRegion, BottomRow } from "./regions";
 import { HIDE_APPS, HIDE_BACKGROUND_OVERLAY, HIDE_VISIBILITY } from "./utils";
 
+const ContextMemo = memo(({ appsRef, mainViewRef }) => (
+	<>
+		<LogoOverlay />
+		<div
+			id="main-view"
+			className={`${HIDE_VISIBILITY} ${HIDE_BACKGROUND_OVERLAY}`}
+			ref={mainViewRef}
+		>
+			<Backgrounds />
+			<div className={`apps ${HIDE_APPS}`} ref={appsRef}>
+				<ModalBase />
+				<div className="region top-bar"></div>
+				<TopRow />
+				<div className="region center-above"></div>
+				<CenterRegion />
+				<CenterBelowRegion />
+				<BottomRow />
+			</div>
+		</div>
+	</>
+));
+
 const App = () => {
-	const { mainViewRef, appsRef } = useUserCustomization();
+	const { appsRef, mainViewRef } = useUserCustomization();
 
 	useAuthPersist();
 
-	return (
-		<>
-			<LogoOverlay />
-			<div
-				id="main-view"
-				className={`${HIDE_VISIBILITY} ${HIDE_BACKGROUND_OVERLAY}`}
-				ref={mainViewRef}
-			>
-				<Backgrounds />
-				<div className={`apps ${HIDE_APPS}`} ref={appsRef}>
-					<ModalBase />
-					<div className="region top-bar"></div>
-					<TopRow />
-					<div className="region center-above"></div>
-					<CenterRegion />
-					<CenterBelowRegion />
-					<BottomRow />
-				</div>
-			</div>
-		</>
-	);
+	return <ContextMemo appsRef={appsRef} mainViewRef={mainViewRef} />;
 };
 
 export default App;
