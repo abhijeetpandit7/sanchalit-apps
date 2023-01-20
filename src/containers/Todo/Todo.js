e6ac4f479c75bd4417d2a2e8db470f59999bfc23
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2023-01-20 15:28:58 +0530

    Add ContextMemo in Todo

--- src/containers/Todo/Todo.js
+++ src/containers/Todo/Todo.js
@@ -1,7 +1,49 @@
-import React, { lazy, Suspense } from "react";
-import { useUserActions, useUserCustomization } from "../../hooks";
+import React, {
+	lazy,
+	memo,
+	Suspense,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
+import {
+	FocusOutHandler,
+	useUserActions,
+	useUserCustomization,
+} from "../../hooks";
+import { NIPPLE, NIPPLE_BOTTOM_RIGHT, TODO, toggleAppPopup } from "../../utils";
 
-const ContextMemo = lazy(() => import("./ContextMemo"));
+const App = lazy(() => import("./App"));
+
+const ContextMemo = memo(({ setWidgetReady }) => {
+	const todoRef = useRef(null);
+	const [componentDidMount, setComponentDidMount] = useState(false);
+
+	useEffect(() => setWidgetReady({ widget: TODO }), []);
+
+	// TODO: Conditional variable: Stay open
+	FocusOutHandler({ ref: todoRef });
+
+	const toggleTodoApp = () => {
+		toggleAppPopup(todoRef);
+		setComponentDidMount(true);
+	};
+
+	return (
+		<div id="todo" className="app-container todo" ref={todoRef}>
+			<div className={`app-wrapper ${NIPPLE} ${NIPPLE_BOTTOM_RIGHT}`}>
+				{componentDidMount && (
+					<Suspense fallback={null}>
+						<App />
+					</Suspense>
+				)}
+			</div>
+			<span className="app-dash toggle Todo-toggle" onClick={toggleTodoApp}>
+				Todo
+			</span>
+		</div>
+	);
+});
 
 export const Todo = () => {
 	const {
@@ -9,13 +51,5 @@ export const Todo = () => {
 	} = useUserCustomization();
 	const { setWidgetReady } = useUserActions();
 
-	return (
-		<>
-			{todoVisible && (
-				<Suspense fallback={null}>
-					<ContextMemo {...{ setWidgetReady }} />
-				</Suspense>
-			)}
-		</>
-	);
+	return <>{todoVisible && <ContextMemo {...{ setWidgetReady }} />}</>;
 };
